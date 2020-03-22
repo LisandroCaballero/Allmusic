a7b627c44f0aec80a1859b0e7ff81ff322e2632e
Lisandro Caballero <lcaballero@urbano.com.ar>
2020-03-22 20:29:41 -0300

    Se agregaron componets home, search y artista y router.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { ROUTES} from './app.routers';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistaComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true} )
   ],
   providers: [],
   bootstrap: [AppComponent]
